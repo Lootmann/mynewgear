9aa33a622232091e251cdb9cf1f705f0c9c290d6
lootmann <chore.machines.10260930@gmail.com>
2025-03-30 06:51:18 +0900

    add Link to Top page and add Add/All Clear button with no feature

--- src/counter/Counter.tsx
+++ src/counter/Counter.tsx
@@ -1,3 +1,33 @@
+import { Link } from "react-router-dom";
+
+const button = `border-2 px-2 rounded-md`;
+
 export default function Counter() {
-  return <div className="p-4">Counter</div>;
+  return (
+    <div className="p-4">
+      <header className="flex gap-4 align-middle justify-center">
+        <Link to="/">
+          <h1 className="text-2xl">BackToBack</h1>
+        </Link>
+
+        <button
+          className={`${button}
+          bg-green-800 border-green-800
+          hover:bg-green-400 hover:border-green-400 hover:text-neutral-800
+          transition delay-75`}
+        >
+          Add
+        </button>
+
+        <button
+          className={`${button}
+        bg-amber-800 border-amber-800
+        hover:bg-amber-400  hover:border-amber-400 hover:text-neutral-800
+        transition delay-75`}
+        >
+          AllClear
+        </button>
+      </header>
+    </div>
+  );
 }
