5239bcbebdbb398f48d88fe0b7036028b7d22119
lootmann <chore.machines.10260930@gmail.com>
2025-03-30 04:18:04 +0900

    renew App.tsx, and global css style

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import "./App.css";
-
 function App() {
   return (
-    <>
-      <div>
-        <h1>Hello World :D</h1>
+    <div className="h-full bg-neutral-900 text-neutral-200">
+      <header className="flex justify-center border-b-neutral-500 border-b-2">
+        <h1 className="text-3xl py-4">Hello World :D</h1>
+      </header>
+
+      <div className=" p-10">
+        <p>hello world</p>
       </div>
-    </>
+    </div>
   );
 }
 
