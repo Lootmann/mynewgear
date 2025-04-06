fb899a56384f27f2a62e98de3dbccbe6c347b4e7
lootmann <chore.machines.10260930@gmail.com>
2025-04-06 22:18:27 +0900

    test: change BrowserRouter to HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import Main from "./Main.tsx";
 import CounterMain from "./counter/Main";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter basename="/mynewgear">
+    <HashRouter basename="">
       <main className="h-full bg-neutral-900 text-neutral-200">
         <Routes>
           <Route path="/" element={<Main />}></Route>
           <Route path="/counter" element={<CounterMain />}></Route>
         </Routes>
       </main>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
