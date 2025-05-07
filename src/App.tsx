ffd763faf99fd2fa084d1ab48a6f29b1810ebd09
lootmann <chore.machines.10260930@gmail.com>
2025-05-07 23:56:36 +0900

    add link to Random CPU

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,18 @@ import { createRoot } from "react-dom/client";
 import { HashRouter, Route, Routes } from "react-router-dom";
 import Main from "./Main.tsx";
 import CounterMain from "./counter/Main";
+import RandomCPUMain from "./randomcpu/Main";
 import "/src/styles/base.css";
 import "/src/styles/app.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <HashRouter basename="">
-      <main className="h-full bg-neutral-300 text-neutral-800">
+      <main className="h-full bg-neutral-900 text-neutral-200">
         <Routes>
           <Route path="/" element={<Main />}></Route>
           <Route path="/counter" element={<CounterMain />}></Route>
+          <Route path="/randomcpu" element={<RandomCPUMain />}></Route>
         </Routes>
       </main>
     </HashRouter>
