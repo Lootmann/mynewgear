2bb7e0b59d6f62b7eb64535ebc42fd5df63c4082
lootmann <chore.machines.10260930@gmail.com>
2025-05-08 00:39:08 +0900

    Main.tsx
    add Link to Random CPU

--- src/Main.tsx
+++ src/Main.tsx
@@ -11,10 +11,20 @@ function Main() {
         <h1 className="text-3xl py-4">Hello World :D</h1>
       </header>
 
-      <div className="p-4">
-        <Link to="/counter">
-          <button className={button}>Link to Counter</button>
-        </Link>
+      <div className="px-10 py-6">
+        <header className="mb-4 flex gap-4">
+          <Link to="/counter">
+            <button className={button}>Link to Counter</button>
+          </Link>
+
+          <Link to="/randomcpu">
+            <button className={button}>Random CPU</button>
+          </Link>
+        </header>
+
+        <div className="">
+          <p>This is my very webSite D;</p>
+        </div>
       </div>
     </>
   );
