65c942295681e64e6be618e261d76b68e6c28bf6
lootmann <chore.machines.10260930@gmail.com>
2025-06-24 02:24:27 +0900

    add saveStorage

--- src/newcpucounter/Storage.ts
+++ src/newcpucounter/Storage.ts
@@ -1,13 +1,18 @@
-import { initRecords, RecordType } from "./Records";
+import { RecordType } from "../types/record";
+import { initRecords } from "./Records";
+
+export function saveStorage(storage_id: string, record: RecordType) {
+  localStorage.setItem(storage_id, JSON.stringify(record));
+}
 
 export function loadStorage(storage_id: string) {
   const storedData = localStorage.getItem(storage_id);
 
   if (storedData === null) {
     const record = initRecords();
-    localStorage.setItem(storage_id, JSON.stringify(record));
+    saveStorage(storage_id, record);
     return record;
   } else {
-    return JSON.parse(storedData) as RecordType[];
+    return JSON.parse(storedData) as RecordType;
   }
 }
