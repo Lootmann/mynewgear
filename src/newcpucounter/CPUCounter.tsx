d89f45c797a2f9414f323b3c58e8de13accd81ca
lootmann <chore.machines.10260930@gmail.com>
2025-06-24 02:33:08 +0900

    does Beautify without useState and useEffect

--- src/newcpucounter/CPUCounter.tsx
+++ src/newcpucounter/CPUCounter.tsx
@@ -1,5 +1,4 @@
-import React from "react";
-import { LevelType } from "./Records";
+import { LevelDataType, LevelType } from "../types/record";
 
 export function CPUCounter({
   myCharId,
@@ -10,63 +9,39 @@ export function CPUCounter({
 }: {
   myCharId: number;
   cpuCharId: number;
-  record: LevelType;
+  record: LevelDataType;
   plusCounter: (
     myCharId: number,
     cpuCharId: number,
     player: "me" | "cpu",
-    level: "lv5" | "lv6" | "lv7" | "lv8"
+    level: LevelType
   ) => void;
   minusCounter: (
     myCharId: number,
     cpuCharId: number,
     player: "me" | "cpu",
-    level: "lv5" | "lv6" | "lv7" | "lv8"
+    level: LevelType
   ) => void;
 }) {
-  const [records, _] = React.useState(() => {
-    let rec: any = [];
-
-    rec.push(
-      {
-        key: "Lv5",
-        score: [record.lv5[0], record.lv5[1]],
-      },
-      {
-        key: "Lv6",
-        score: [record.lv6[0], record.lv6[1]],
-      },
-      {
-        key: "Lv7",
-        score: [record.lv7[0], record.lv7[1]],
-      },
-      {
-        key: "Lv8",
-        score: [record.lv8[0], record.lv8[1]],
-      }
-    );
-    return rec;
-  });
-
-  // handle plus button function
   function handlePlusButton(
     myCharId: number,
     cpuCharId: number,
     player: "me" | "cpu",
-    level: "lv5" | "lv6" | "lv7" | "lv8"
+    level: LevelType
   ) {
     plusCounter(myCharId, cpuCharId, player, level);
   }
 
-  // handle minus button function
   function handleMinusButton(
     myCharId: number,
     cpuCharId: number,
     player: "me" | "cpu",
-    level: "lv5" | "lv6" | "lv7" | "lv8"
+    level: LevelType
   ) {
     minusCounter(myCharId, cpuCharId, player, level);
   }
+
+  const levels: LevelType[] = ["lv5", "lv6", "lv7", "lv8"];
 
   return (
     <div className="flex flex-col">
@@ -74,24 +49,26 @@ export function CPUCounter({
         <h1>CPU Counter</h1>
       </header>
 
-      <div className="grid grid-rows-2 grid-cols-2 mx-20  text-center">
-        {records.map((rec: any) => {
+      <div className="grid grid-rows-2 grid-cols-2 mx-20 text-center gap-2">
+        {levels.map((level) => {
+          const score = record[level];
           return (
             <div
-              key={rec.key}
+              key={level}
               className="grid grid-rows-2 grid-cols-3 border text-xl py-1"
             >
               <div className="row-span-2 border-r flex items-center justify-center">
-                <p>{rec.key}</p>
+                <p>{level}</p>
               </div>
-              <div>{rec.score[0]}</div>
-              <div>{rec.score[1]}</div>
+
+              <div>{score[0]}</div>
+              <div>{score[1]}</div>
 
               <div className="flex justify-around">
                 <button
                   className="border px-1 rounded-md bg-blue-900 hover:bg-blue-400"
                   onClick={() =>
-                    handlePlusButton(myCharId, cpuCharId, "me", rec.key)
+                    handlePlusButton(myCharId, cpuCharId, "me", level)
                   }
                 >
                   ＋
@@ -99,7 +76,7 @@ export function CPUCounter({
                 <button
                   className="border px-1 rounded-md bg-green-900 hover:bg-green-500"
                   onClick={() =>
-                    handleMinusButton(myCharId, cpuCharId, "me", rec.key)
+                    handleMinusButton(myCharId, cpuCharId, "me", level)
                   }
                 >
                   －
@@ -110,7 +87,7 @@ export function CPUCounter({
                 <button
                   className="border px-1 rounded-md bg-blue-900 hover:bg-blue-400"
                   onClick={() =>
-                    handlePlusButton(myCharId, cpuCharId, "cpu", rec.key)
+                    handlePlusButton(myCharId, cpuCharId, "cpu", level)
                   }
                 >
                   ＋
@@ -118,7 +95,7 @@ export function CPUCounter({
                 <button
                   className="border px-1 rounded-md bg-green-900 hover:bg-green-500"
                   onClick={() =>
-                    handleMinusButton(myCharId, cpuCharId, "cpu", rec.key)
+                    handleMinusButton(myCharId, cpuCharId, "cpu", level)
                   }
                 >
                   －
