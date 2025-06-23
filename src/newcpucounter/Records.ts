c6695546c4de29794087c535ba5bfffbac8583e2
lootmann <chore.machines.10260930@gmail.com>
2025-06-24 02:28:49 +0900

    Record function

--- src/newcpucounter/Records.ts
+++ src/newcpucounter/Records.ts
@@ -1,34 +1,15 @@
 import { CHARACTERS } from "../global/characters";
+import {
+  CharacterNameType,
+  CharacterRecordType,
+  RecordType,
+} from "../types/record";
 
-/*
-[
-    {
-        id:1,
-        "char1": {
-            "char1": { lv5: [], lv6: [], lv7: [], lv8: [] },
-            "char2": { lv5: [], lv6: [], lv7: [], lv8: [] },
-        },
-    },
-    ...
-]
-*/
-type Level = "lv5" | "lv6" | "lv7" | "lv8";
-export type LevelType = { [key in Level]: [number, number] };
-type CharacterName = (typeof CHARACTERS)[number]["name"];
-// "dj": { lv5: [0,0], lv6: [0,0], lv7: [0,0], lv8: [0,0] },
-export type CharacterType = { [key in CharacterName]: LevelType };
-export type SelectedCharacterType = CharacterType;
-export type RecordType = {
-  id: number;
-  [key: string]: CharacterType | number;
-};
-
-export function initRecords() {
-  let records: RecordType[] = [];
+export function initRecords(): RecordType {
+  let records: RecordType = {};
 
   for (const charMain of CHARACTERS) {
-    let characterRecord: CharacterType = {} as CharacterType;
-
+    let characterRecord: CharacterRecordType = {} as CharacterRecordType;
     for (const charEnemy of CHARACTERS) {
       characterRecord[charEnemy.name] = {
         lv5: [0, 0],
@@ -37,12 +18,14 @@ export function initRecords() {
         lv8: [0, 0],
       };
     }
-
-    records.push({
-      id: charMain.id,
-      [charMain.name]: characterRecord,
-    });
+    records[charMain.name] = characterRecord;
   }
 
   return records;
+}
+
+export function CharNameFindByid(charId: number): CharacterNameType {
+  const char = CHARACTERS.find((char) => char.id == charId);
+  if (!char) return "aki";
+  else return char.name;
 }
